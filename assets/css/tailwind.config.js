1a5c932f8076561a564d22e1fb1ef7515b7b97bc
Armoghan <Armoghan16@gmail.com>
2024-01-09 18:12:35 +0500

    Added Default Theme

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -9,7 +9,34 @@ module.exports = {
         "./content/**/*.html"
     ],
     theme: {
-        extend: {},
+        fontFamily: {
+            'heading': 'var(--heading)',
+            'body': 'var(--body)'
+        },
+        extend: {
+            colors: {
+                primary: 'rgb(var(--primary))',
+                secondary: 'rgb(var(--secondary))',
+                accent: 'rgb(var(--accent))',
+                neutral: 'rgb(var(--neutral))',
+                base: 'rgb(var(--base))',
+                info: 'rgb(var(--info))',
+                success: 'rgb(var(--success))',
+                warning: 'rgb(var(--warning))',
+                error: 'rgb(var(--error))'
+            },
+            backgroundColor: {
+                'primary': 'rgba(var(--primary) / var(--tw-border-opacity))',
+                'secondary': 'rgba(var(--secondary)/ var(--tw-border-opacity))',
+                'accent': 'rgba(var(--accent) / var(--tw-border-opacity))',
+                'neutral': 'rgba(var(--neutral) / var(--tw-border-opacity))',
+                'base': 'rgba(var(--base) / var(--tw-border-opacity))',
+                'info': 'rgba(var(--info) / var(--tw-border-opacity))',
+                'success': 'rgba(var(--success) / var(--tw-border-opacity))',
+                'warning': 'rgba(var(--warning) / var(--tw-border-opacity))',
+                'error': 'rgba(var(--error) / var(--tw-border-opacity))'
+            }
+        },
     },
     plugins: [
         require('flowbite/plugin'),
